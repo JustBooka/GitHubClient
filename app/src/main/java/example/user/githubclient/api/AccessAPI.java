9ea04b1d3aff5476e23a40a1addb0909e6bfe0b0
alexey.bukin@e-legion.com <HGz8HwR`b>
2015-03-22 16:09:57 +0300

    change access request

--- app/src/main/java/example/user/githubclient/api/AccessAPI.java
+++ app/src/main/java/example/user/githubclient/api/AccessAPI.java
@@ -5,17 +5,18 @@ import java.util.List;
 import example.user.githubclient.models.Access;
 import retrofit.Callback;
 import retrofit.http.GET;
-import retrofit.http.Header;
+import retrofit.http.Headers;
+import retrofit.http.Path;
 import retrofit.http.Query;
 
 /**
  * Created by Just on 3/21/2015.
  */
 public interface AccessAPI {
-
-    @GET("/access_token?client_id=b89e0192e6b3c9bbd9b8&client_secret=c3e6ed559361f528cb6c4e6e5040d31459236698&")
-    public void GetAccess(
-//                              @Path("clientId") String clientId,@Path("clientSecret") String clientSecret
-             @Query("code") String code,
+    @Headers({
+            "Accept: application/json"
+    })
+    @GET("/access_token{secretAndId}{code}")
+    public void GetAccess(@Query("secrertAndId") String secretId, @Query("code") String code,
                           Callback<List<Access>> response);
 }
